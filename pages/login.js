27682217418c7b7673ab42306def85e9883761a1
prasetion <prasetion@gmail.com>
2023-03-05 11:35:17 +0700

    setup pages

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const login = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>login</h1>
+    </div>
+  );
+};
+
+export default login;
---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+const register = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <h1>register</h1>
+    </div>
+  );
+};
+
+export default register;
