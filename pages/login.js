c8cbc5c3a4e94ea8a07ad665f38ddba996e9d29b
prasetion <prasetion@gmail.com>
2023-03-05 21:27:47 +0700

    login page layout

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Layout from "@/layout/layout";
+import Link from "next/link";
 
 const login = () => {
   return (
@@ -9,7 +10,37 @@ const login = () => {
         <Head>
           <title>Login</title>
         </Head>
-        <h1>login</h1>
+        <section className="w-3/4 mx-auto flex flex-col gap-10">
+          <div className="title">
+            <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
+            <p className="w-3/4 mx-auto text-gray-400">How to login</p>
+          </div>
+
+          <form action="" className="flex flex-col gap-5">
+            <div className=" input-group">
+              <input type="email" name="email" placeholder="email" />
+            </div>
+            <div className=" input-group">
+              <input type="password" name="password" placeholder="password" />
+            </div>
+
+            <div className="input-button">
+              <button type="submit">Login</button>
+            </div>
+            <div className="input-button">
+              <button type="submit">Sign In with Google</button>
+            </div>
+            <div className="input-button">
+              <button type="submit">Sign In with Github</button>
+            </div>
+          </form>
+          <p className="text-center text-gray-400">
+            don't have an account yet?{" "}
+            <Link href={"/register"} className="text-blue-700">
+              Sign Up
+            </Link>
+          </p>
+        </section>
       </Layout>
     </div>
   );
