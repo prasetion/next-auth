70ea33dcea6326cabe5a90f8423204376eff7aff
prasetion <prasetion@gmail.com>
2023-05-17 18:22:33 +0700

    profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { getSession } from "next-auth/react";
 
-export default () => {
+import React from "react";
+
+const profile = () => {
   return (
     <section className="container mx-auto text-center">
       <h3 className="text-4xl font-bold">Profile Page</h3>
@@ -10,6 +12,18 @@ export default () => {
     </section>
   );
 };
+
+export default profile;
+
+// export default () => {
+//   return (
+//     <section className="container mx-auto text-center">
+//       <h3 className="text-4xl font-bold">Profile Page</h3>
+
+//       <Link href={"/"}>Home Page</Link>
+//     </section>
+//   );
+// };
 
 export async function getServerSideProps({ req }) {
   const session = await getSession({ req });
